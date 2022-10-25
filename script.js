1fd21a66a901bc5911918b559e70e9b771eaa2fe
Arnor Ameti <arnameti@gmail.com>
2022-10-25 15:11:41 +0200

    Add Event Listener

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+'use strict';
 
+const calcButtons = document.querySelector('.calculator__buttons');
+const calcTitle = document.querySelector('.calculator__title');
+
+calcButtons.addEventListener('click', function (e) {
+    const clicked = e.target.closest('.button')
+
+    if (!clicked) return;
+
+    const displayValue = calcTitle.textContent;
+
+    calcTitle.textContent = calcTitle.textContent === '0' ? clicked.value : displayValue + clicked.value;
+});
